846db075379be212ac4f352d3e6b18b6e0c94afe
Farhad <farhaduneci@gmail.com>
2022-01-09 15:50:40 +0330

    Project configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  purge: [
-    './_includes/**/*.html',
-    './_layouts/**/*.html',
-    './_posts/*.html',
-    './*.html'
+  content: [
+    './site/**/*.{html,js,md}'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'roboto': ['Roboto Slab', 'sans-serif', 'serif'],
+    },
     extend: {}
   },
   variants: {
