4ae51b058ed9ab2b34a100fb238b97d361d4ef95
Farhad <farhaduneci@gmail.com>
2022-01-10 11:43:49 +0330

    Add background pattern

--- Gemfile
+++ Gemfile
@@ -1,5 +1,9 @@
 source "https://rubygems.org"
 
 gem "jekyll"
-gem 'jekyll-seo-tag'
-gem 'jekyll-minifier'
+
+group :jekyll_plugins do
+    gem 'jekyll-seo-tag'
+    gem 'jekyll-minifier'
+    gem 'jekyll-timeago'
+end
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,12 @@ module.exports = {
     letterSpacing: {
       wide: '5px',
     },
-    extend: {}
+    extend: {
+      backgroundImage: {
+        'pattern': 'url("/assets/images/Bg.svg")',
+        'card-pattern': "linear-gradient(to top, rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.9)), url('/assets/images/CardBg.svg')",
+      },
+    }
   },
   variants: {
     extend: {}
