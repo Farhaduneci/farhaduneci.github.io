3e5b8c125cb270c24f0ccfdab3cfa896f6c8f0a5
Farhad <farhaduneci@gmail.com>
2023-01-10 11:27:05 +0330

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./site/**/*.{html,js,md}"],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      roboto: ["Roboto Slab", "sans-serif", "serif"],
+    },
+    letterSpacing: {
+      wide: "5px",
+    },
+    extend: {
+      backgroundImage: {
+        pattern: 'url("/assets/images/Bg.svg")',
+        "card-pattern":
+          "linear-gradient(to top, rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.9)), url('/assets/images/CardBg.svg')",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
